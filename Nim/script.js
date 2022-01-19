0e7e8293c1b75719598a4ffa143ff3122c9820f0
DanteGutierrez <78101352+DanteGutierrez@users.noreply.github.com>
2022-01-19 10:46:12 -0700

    Update script.js

--- Nim/script.js
+++ Nim/script.js
@@ -22,51 +22,53 @@ let starThree = document.getElementById("StarThree");
 //Global Variables
 let difficulty = 0;
 let playerTurn = true; //True = P1 | False = P2
+let row_selected;
+var difficulties = {
+    0: [1, 3, 5],
+    1: [1, 3, 5, 7],
+    2: [3, 5, 7, 9, 11]
+};
 
 //Method
 const TurnSwitch = () => {
     playerPicture.src = playerTurn ? "media/Whale.png" : "media/BretIcon.png";
     playerPicture.alt = playerTurn ? "Player 1 Icon" : "Player 2 Icon";
     playerTurnLabel.innerHTML = playerTurn ? "Player 1's Turn" : "Player 2's Turn";
+    row_selected = null;
 }
+
 const DifficultySwitch = StarId => {
     if (StarId !== "StarOne" && StarId !== "StarTwo" && StarId !== "StarThree") {
         console.log("fixed ya");
         return;
-    }; 
-//Event Listeners
-instructionsCloseButton.addEventListener("click", evt => {
-    instructionsHide.hidden = true;
-    gameHide.hidden = false;
-});
-
-instructionsOpenButton.addEventListener("click", evt => {
-    instructionsHide.hidden = false;
-    gameHide.hidden = true;
-});
-
-resetButton.addEventListener("click", evt => {
-    setup_board(difficulty);
-});
-
-difficultyContainer.addEventListener("click", evt => {
-    DifficultySwitch(evt.path[0].id);
-});
-
-//Run on StartUp
-TurnSwitch();
-DifficultySwitch("StarOne");
-setup_board(difficulty);
-
-let turn = true;
-let row_selected;
-var difficulties = {
-    0: [1, 3, 5],
-    1: [1, 3, 5, 7],
-    2: [3, 5, 7, 9, 11]
-}
-
-let board = setup_board();
+    };
+    starOne.classList.remove("star-on");
+    starTwo.classList.remove("star-on");
+    starThree.classList.remove("star-on");
+    starOne.classList.remove("star-off");
+    starTwo.classList.remove("star-off");
+    starThree.classList.remove("star-off");
+    switch (StarId) {
+        case "StarOne":
+            starOne.classList.add("star-on");
+            starTwo.classList.add("star-off");
+            starThree.classList.add("star-off");
+            break;
+        case "StarTwo":
+            starOne.classList.add("star-on");
+            starTwo.classList.add("star-on");
+            starThree.classList.add("star-off");
+            break;
+        case "StarThree":
+            starOne.classList.add("star-on");
+            starTwo.classList.add("star-on");
+            starThree.classList.add("star-on");
+            break;
+        default:
+            console.log("There was a bit of an issue");
+            break;
+    }
+};
 
 const setup_board = () => {
     board = reset_game();
@@ -99,7 +101,32 @@ const game_over = () => {
     }
 }
 
-const switch_turns = () => {
-    turn = !turn;
-    row_selected = null;
-}
+//Event Listeners
+instructionsCloseButton.addEventListener("click", evt => {
+    instructionsHide.hidden = true;
+    gameHide.hidden = false;
+});
+
+instructionsOpenButton.addEventListener("click", evt => {
+    instructionsHide.hidden = false;
+    gameHide.hidden = true;
+});
+
+resetButton.addEventListener("click", evt => {
+    setup_board(difficulty);
+});
+
+difficultyContainer.addEventListener("click", evt => {
+    DifficultySwitch(evt.path[0].id);
+});
+
+//Run on StartUp
+TurnSwitch();
+DifficultySwitch("StarOne");
+setup_board(difficulty);
+let board = setup_board();
+
+
+
+
+
