224cf0e4648a3708c0918cb323c6f8161f23cb11
DanteGutierrez <78101352+DanteGutierrez@users.noreply.github.com>
2022-01-19 10:25:48 -0700

    changed some things, fixing layout

--- Nim/script.js
+++ Nim/script.js
@@ -11,6 +11,8 @@ let playerTwoInput = document.getElementById("PlayerTwoInput");
 //Ids
 let instructionsHide = document.getElementById("InstructionsHide");
 let gameHide = document.getElementById("GameHide");
+let playerPicture = document.getElementById("PlayerPicture");
+let playerTurnLabel = document.getElementById("PlayerTurnLabel");
 
 //Stars
 let starOne = document.getElementById("StarOne");
@@ -19,27 +21,26 @@ let starThree = document.getElementById("StarThree");
 
 //Global Variables
 let difficulty = 0;
+let playerTurn = true; //True = P1 | False = P2
 
-//Event Listeners
-instructionsCloseButton.addEventListener("click", evt => {
-    instructionsHide.hidden = true;
-    gameHide.hidden = false;
-});
-instructionsOpenButton.addEventListener("click", evt => {
-    instructionsHide.hidden = false;
-    gameHide.hidden = true;
-});
-resetButton.addEventListener("click", evt => {
-
-});
-difficultyContainer.addEventListener("click", evt => {
+//Method
+const TurnSwitch = () => {
+    playerPicture.src = playerTurn ? "media/Whale.png" : "media/BretIcon.png";
+    playerPicture.alt = playerTurn ? "Player 1 Icon" : "Player 2 Icon";
+    playerTurnLabel.innerHTML = playerTurn ? "Player 1's Turn" : "Player 2's Turn";
+}
+const DifficultySwitch = StarId => {
+    if (StarId !== "StarOne" && StarId !== "StarTwo" && StarId !== "StarThree") {
+        console.log("fixed ya");
+        return;
+    }; 
     starOne.classList.remove("star-on");
     starTwo.classList.remove("star-on");
     starThree.classList.remove("star-on");
     starOne.classList.remove("star-off");
     starTwo.classList.remove("star-off");
     starThree.classList.remove("star-off");
-    switch (evt.path[0].id) {
+    switch (StarId) {
         case "StarOne":
             starOne.classList.add("star-on");
             starTwo.classList.add("star-off");
@@ -59,4 +60,24 @@ difficultyContainer.addEventListener("click", evt => {
             console.log("There was a bit of an issue");
             break;
     }
+}
+
+//Event Listeners
+instructionsCloseButton.addEventListener("click", evt => {
+    instructionsHide.hidden = true;
+    gameHide.hidden = false;
 });
+instructionsOpenButton.addEventListener("click", evt => {
+    instructionsHide.hidden = false;
+    gameHide.hidden = true;
+});
+resetButton.addEventListener("click", evt => {
+
+});
+difficultyContainer.addEventListener("click", evt => {
+    DifficultySwitch(evt.path[0].id);
+});
+
+//Run on StartUp
+TurnSwitch();
+DifficultySwitch("StarOne");
