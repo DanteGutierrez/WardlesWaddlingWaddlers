d001fdec9b963be6a74b69423fe392e10aec26e9
DanteGutierrez <78101352+DanteGutierrez@users.noreply.github.com>
2022-01-26 09:10:57 -0700

    Comments for you guys

--- Pente/scripts.js
+++ Pente/scripts.js
@@ -8,12 +8,14 @@ const ghost = document.getElementById("Ghost");
 
 
 
-//Consts
+//Board Customization Consts
 const outsideMargin = 0;
 const boardMargin = 30;
 const cellSize = 40;
 const boardSize = 19;
-const COLORS = Object.freeze({
+
+// JS Enum Equivalent
+const COLORS = Object.freeze({ 
     BLACK: 'black',
     BLUE: 'blue',
     GREEN: 'green',
@@ -30,87 +32,125 @@ let currentPlayer = true; //True = p1 || False = p2
 let playerOneColor = COLORS.WHITE;
 let playerTwoColor = COLORS.BLACK;
 
-//Find Board Coord
-const findBoardCoord = ArrayCoord => {
+//Array Coord => Pixel Coord
+const findPixelCoord = ArrayCoord => {
     // Takes either Array Row number or Array Y number and turns it into pixel measurement
     return (ArrayCoord * cellSize) + boardMargin + outsideMargin + (cellSize / 2);
 }
 
+//Pixel Coord => Array Coord
+const findArrayCoord = PixelCoord => {
+    //Takes either Pixel X or Pixel Y and turns it into Array Coord
+    return Math.floor((PixelCoord - outsideMargin - boardMargin - (cellSize / 2)) / cellSize);
+}
+
 //Generate Board
 const generateBoard = () => {
-    // r = row || c = column
+    //Clear Board
     let board = "";
-    for (let r = 0; r < boardSize + 1; r++) {
+
+    //Tie rows and columns of visual tiles
+    for (let row = 0; row < boardSize + 1; row++) {
         board += "<div class='container horizontal'>";
-        for (let c = 0; c < boardSize + 1; c++) {
+        for (let col = 0; col < boardSize + 1; col++) {
             board += `<div class="boardCell"></div>`;
         }
         board += "</div>";
     }
+
+    //Add all tiles to board
     gameBoardVisual.innerHTML = board;
-    for (let r = 0; r < boardSize; r++) {
-        boardArray[r] = [];
-        for (let c = 0; c < boardSize; c++) {
-            boardArray[r][c] = ""; //Generates a blank board
+
+    //Go through all tiles of the board and clear out pieces
+    for (let row = 0; row < boardSize; row++) {
+        boardArray[row] = [];
+        for (let col = 0; col < boardSize; col++) {
+            boardArray[row][col] = "";
         }
     }
-    console.log(boardArray);
+
+    //console.log(boardArray);
 };
 
 //Render the Board
 const renderBoard = () => {
+    //Clear pieces
     gameBoardPhysical.innerHTML = "";
-    // r = row || c = column
-    for (let r = 0; r < boardArray.length; r++) {
-        for (let c = 0; c < boardArray[r].length; c++) {
-            if (boardArray[r][c] !== "") {
-                let imageString = "assets/" + boardArray[r][c] + ".png"
-                x = findBoardCoord(r);
-                y = findBoardCoord(c);
+
+    //Iterate through board and place every piece
+    for (let row = 0; row < boardArray.length; row++) {
+        for (let col = 0; col < boardArray[row].length; col++) {
+            if (boardArray[row][col] !== "") {
+
+                //Turn color into png string
+                let imageString = "assets/" + boardArray[row][col] + ".png"
+
+                //Find out where to place the piece
+                x = findPixelCoord(row);
+                y = findPixelCoord(col);
+                
+                //Generate the piece at calculated coords with image
                 gameBoardPhysical.innerHTML += `<img src=${imageString} style="left:${x}px; top:${y}px;"/>`;
             }
         }
     }
 }
 
-//Click
+//Place Piece Click
 gameBoard.addEventListener("click", evt => {
+
     //Grab Current Cursor Coords
     let x = evt.clientX;
     let y = evt.clientY;
-    console.log(`True Coords: ${x}, ${y}`);
+    // console.log(`True Coords: ${x}, ${y}`);
+
     //Tells what tile you are on, in terms of the array
-    let aX = Math.floor((x - outsideMargin - boardMargin - (cellSize / 2)) / cellSize);
-    let aY = Math.floor((y - outsideMargin - boardMargin - (cellSize / 2)) / cellSize);
-    console.log(`Assumed Cell: ${aX}, ${aY}`);
+    let arrayX = findArrayCoord(x);
+    let arrayY = findArrayCoord(y);
+    // console.log(`Assumed Cell: ${arrayX}, ${arrayY}`);
+
     //Set Color in place
-    boardArray[aX][aY] = (currentPlayer ? playerOneColor : playerTwoColor); 
+    boardArray[arrayX][arrayY] = (currentPlayer ? playerOneColor : playerTwoColor); 
+
+    //Update Board
     renderBoard();
+
     //Change Player Turn
     currentPlayer = !currentPlayer;
     
 });
 
-//Ghost Hover
+//Ghost Hover Piece
 gameBoard.addEventListener("mousemove", evt => {
+
     //Grab Current Cursor Coords
     let x = evt.clientX;
     let y = evt.clientY;
-    //Tells what tile you are on, in terms of the array
-    let aX = Math.floor((x - outsideMargin - boardMargin - (cellSize / 2)) / cellSize);
-    let aY = Math.floor((y - outsideMargin - boardMargin - (cellSize / 2)) / cellSize);
-    if ((aX >= 0 && aX <= boardSize - 1) && (aY >= 0 && aY <= boardSize - 1)) { //If you are within the bounds of the array, display the ghost
-        //Ghost math
-        let bX = findBoardCoord(aX);
-        let bY = findBoardCoord(aY);
 
-        ghost.src = "assets/" + (currentPlayer ? playerOneColor : playerTwoColor) + ".png"; //Change player color
+    //Tells what tile you are on, in terms of the array
+    let arrayX = findArrayCoord(x);
+    let arrayY = findArrayCoord(y);
+
+    //If you are within the bounds of the array, display the ghost
+    if ((arrayX >= 0 && arrayX <= boardSize - 1) && (arrayY >= 0 && arrayY <= boardSize - 1)) { 
+
+        //Ghost math
+        let boardX = findPixelCoord(arrayX);
+        let boardY = findPixelCoord(arrayY);
+
+        //Change player color
+        ghost.src = "assets/" + (currentPlayer ? playerOneColor : playerTwoColor) + ".png"; 
+
         //Move Piece
-        ghost.style.left = `${bX}px`;
-        ghost.style.top = `${bY}px`;
+        ghost.style.left = `${boardX}px`;
+        ghost.style.top = `${boardY}px`;
+
+        //After all other parts of the ghost are set, make the ghost appear
         ghost.hidden = false;
     }
     else {
+
+        //Hide the ghost because you are outside of the board
         ghost.hidden = true;
     }
 });
